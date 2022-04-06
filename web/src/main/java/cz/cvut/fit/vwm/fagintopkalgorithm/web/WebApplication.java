169740b2f61950c9fd25d0e57295b17ce312a648
Jakub Janeček <kuba.rone@gmail.com>
2022-04-06 15:24:15 +0200

    Initial commit for frontend (web)

---
+++ web/src/main/java/cz/cvut/fit/vwm/fagintopkalgorithm/web/WebApplication.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.vwm.fagintopkalgorithm.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplication.class, args);
+	}
+
+}
---
+++ web/src/test/java/cz/cvut/fit/vwm/fagintopkalgorithm/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.vwm.fagintopkalgorithm.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
